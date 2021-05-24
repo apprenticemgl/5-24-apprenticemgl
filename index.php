bae0bbdd0cf9063b02a5362ff672e205ab8a60c1
ApprenticeMGL <apprenticemn@gmail.com>
2021-05-24 19:51:57 +0800

    update

--- index.php
+++ index.php
@@ -33,59 +33,8 @@
     <style>
     /* css ornoo = cascading STYLE sheet*/
     </style>
-
-    <script>
-    /* javaSCRIPT ornoo */
-    // console.log('bi end log bichig baina');
-    // console.error('bi end aldaa gargaj baina');
-
-    // alert('Sanuulga');
-
-    // console.error('alerting daraa aldaa');
-
-    // console.log(variable_name);
-    function submittingForm() {
-        console.log('form submitting');
-    }
-    // $variable_name = 'variable holder';
-    var variable_name = 'variable holder';
-
-    // console.log(variable_name);
-
-    /* end yu bolj bainaa gedegiig tailbalraarai!! */
-    function eneIdtaiElementSoli(elementId) {
-        var el = document.getElementById(elementId);
-        el.innerText = 'xooson';
-        console.log(el);
-    }
-
-    //Method = Function
-
-    function tooBod(variable) {
-        // function happens here
-        console.log(variable, 'tooBod function yavj duuslaa');
-        return variable;
-    }
-
-    // tooBod('zov zov');
-    // tooBod(variable_name);
-
-    var buxButtons = document.getElementsByClassName('btn');
-    tooBod(buxButtons);
-
-    // document.getElementById('with-child').removeChild('p');
-
-
-    function dangerDarsan() {
-        var successButton = document.getElementById('success');
-        console.log(successButton);
-        // alert('succes soligdloo shuu');
-        successButton.innerHTML = 'Hi';
-        successButton.setAttribute('value', 'HI hi');
-        successButton.setAttribute('type', 'text');
-    }
-
-    </script>
+    <script src="jquery.js"></script>
+    <script src="main.js"></script>
 
     <?php
     function tooBod($variable){
---
+++ main.js
@@ -0,0 +1,74 @@
+/* 5/24 class */
+$(document).ready(function(){
+    $("p").click(function(){
+      $(this).hide();
+    });
+
+    
+    console.log('query document ready');
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* javaSCRIPT ornoo * /
+// 5/21
+// console.log('bi end log bichig baina');
+// console.error('bi end aldaa gargaj baina');
+
+// alert('Sanuulga');
+
+// console.error('alerting daraa aldaa');
+
+// console.log(variable_name);
+function submittingForm() {
+    console.log('form submitting');
+}
+// $variable_name = 'variable holder';
+var variable_name = 'variable holder';
+
+// console.log(variable_name);
+
+// end yu bolj bainaa gedegiig tailbalraarai!!
+
+function eneIdtaiElementSoli(elementId) {
+    var el = document.getElementById(elementId);
+    el.innerText = 'xooson';
+    console.log(el);
+}
+
+//Method = Function
+
+function tooBod(variable) {
+    // function happens here
+    console.log(variable, 'tooBod function yavj duuslaa');
+    return variable;
+}
+
+// tooBod('zov zov');
+// tooBod(variable_name);
+
+var buxButtons = document.getElementsByClassName('btn');
+tooBod(buxButtons);
+
+// document.getElementById('with-child').removeChild('p');
+
+
+function dangerDarsan() {
+    var successButton = document.getElementById('success');
+    console.log(successButton);
+    // alert('succes soligdloo shuu');
+    successButton.innerHTML = 'Hi';
+    successButton.setAttribute('value', 'HI hi');
+    successButton.setAttribute('type', 'text');
+}
+*/
